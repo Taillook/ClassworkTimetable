5da8232042a2ca5be5a8dc646db07e5bb4db1e53
Taillook <jousoku@gmail.com>
2017-05-21 22:42:52 +0900

    [Add] Timetable Class

---
+++ ClassworkTimetable/ClassworkTimetable/Timetable.swift
@@ -0,0 +1,14 @@
+//
+//  Timetable.swift
+//  ClassworkTimetable
+//
+//  Created by Taillook on 2017/05/21.
+//  Copyright © 2017年 Taillook. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Timetable: UIView {
+    
+}
